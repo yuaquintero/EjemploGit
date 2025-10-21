22198b79355f61c50003a15b4046976869487d97
Yaquintero <yudy.quintero@udea.edu.co>
2025-10-21 18:41:35 -0500

    Creacion y lectura de variable

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
+    int numero;
     cout << "Este es mi primer repositorio" << endl;
+    cout <<"Ingrese un numero"<<endl;
+    cin>>numero;
     return 0;
 }
